af3ebaae3c42d3615c134d1032aec6da0cd98fa7
flikjo <mabeltran21@poligran.edu.co>
2021-04-07 22:29:09 -0500

    cliente socket

---
+++ cliente_socket/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+app.use(express.json({extended: false}));
+
+
+const io = require('socket.io-client');
+// http://localhost:3009 => direccion del servidor al cual me quiero conectar
+const socket = io("http://18.233.6.37:3009");
+
+app.enable('trust proxy');
+
+const Port = process.env.Port || 3010;
+
+app.listen(Port, () => console.log("server started"));
+
+socket.on("connect", () => {
+    console.log(socket.connected); // true
+});
+
+socket.on("Connect_failed", () => {
+  console.log(socket.connected);
+});
+
+socket.on('respuesta', (respuesta) => {
+    console.log(respuesta);
+});
